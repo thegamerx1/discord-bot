95fb305f557204d20b5dabc656dd412f517663c4
TheGamerX1 <thehackerdroid77@gmail.com>
2021-02-16 13:07:22 +0100

    dasdasd

--- pullall.cmd
+++ pullall.cmd
@@ -1,6 +1,8 @@
 @echo off
+set asda=%~dp0
 git reset --hard origin/master
 git pull origin master
 cd %userprofile%\Documents\AutoHotkey\lib
 git fetch --all
 git reset --hard origin/master
+cd /D %asda%
